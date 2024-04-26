653cc8e9a9bf8ff5c4bafb99f7bac04a1bc72d0d
Suryansh Bhatnagar <sbh2068@gmail.com>
2024-04-26 16:01:41 +0530

    view all button added

--- src/components/Body/Alerts.jsx
+++ src/components/Body/Alerts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AlertCard from './AlertCard'
 import { FaRegUser } from 'react-icons/fa'
 import { MdDeviceThermostat } from "react-icons/md";
-
+import { MdNavigateNext } from "react-icons/md";
 const Alerts = () => {
 
   const alertData=[
@@ -12,7 +12,13 @@ const Alerts = () => {
 
   return (
     <div className='my-6'>
+      <div className='flex justify-between '>
            <p className='font-medium text-base mb-4'> High Priority alerts (14)</p>
+           <div className='flex items-center gap-2 text-blue-900 text-sm'>
+           <p className=''>View All  </p>
+           <MdNavigateNext fontSize={18} />
+           </div>
+      </div>
            <div className='flex w-full gap-16'>
             {
               alertData.map((alert,index)=> <AlertCard icon={alert.icon} bill={alert.bill} description={alert.description} load={alert.load} title={alert.title} key={index}/>)
